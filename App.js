fa6df33d86ef22f8ea7d2142d9081e9246383196
paulmorello <paulmorello1@gmail.com>
2018-07-14 11:12:19 -0400

    completed next stage for countdown app

--- App.js
+++ App.js
@@ -1,24 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+
+// File Imports
+import EventList from './EventList';
 
 export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <EventList />
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
